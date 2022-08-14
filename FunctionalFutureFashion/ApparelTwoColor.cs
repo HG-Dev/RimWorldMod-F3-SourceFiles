e05571f4fd9e29e25c75afb7ee551cbecef3c1a9
HG-Dev <ian.hg.dev@gmail.com>
2022-08-14 20:46:19 +0900

    Initial commit: bug-hunting WIP

---
+++ FunctionalFutureFashion/ApparelTwoColor.cs
@@ -0,0 +1,55 @@
+﻿using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace HG.FFF
+{
+    class ApparelTwoColor : Apparel
+    {
+        public override Color DrawColor
+        {
+            get
+            {
+                Color stdColor = Color.white;
+
+                if (StyleDef != null && StyleDef.color != default(Color))
+                {
+                    stdColor = StyleDef.color;
+                }
+                else
+                {
+                    CompColorable comp = this.GetComp<CompColorable>();
+                    if (comp != null && comp.Active)
+                        return comp.Color;
+                }
+
+                if (stdColor != Color.white)
+                {
+                    Color.RGBToHSV(stdColor, out float hue, out float sat, out float value);
+                    stdColor = Color.HSVToRGB(hue, Mathf.Clamp01(sat * 1.2f), value);
+                }
+
+                return stdColor;
+		    }
+            set
+            {
+                this.SetColor(value, true);
+            }
+        }
+
+        public override Color DrawColorTwo
+        {
+            get
+            {
+                if (this.Stuff != null)
+                    return this.def.GetColorForStuff(this.Stuff);
+
+                if (this.def.graphicData != null)
+                    return this.def.graphicData.colorTwo;
+
+                return Color.white;
+            }
+        }
+    }
+
+}
---
+++ FunctionalFutureFashion/FunctionalFutureFashion.cs
@@ -0,0 +1,22 @@
+﻿using HG.FFF.Harmony;
+using Verse;
+
+namespace HG.FFF
+{
+    [StaticConstructorOnStartup]
+    public class FunctionalFutureFashion : Mod
+    {
+        public static HarmonyLib.Harmony HarmonyInstance;
+
+        public FunctionalFutureFashion(ModContentPack content) : base(content)
+        {
+            HarmonyInstance = new HarmonyLib.Harmony("HG.Originals.F3");
+            LongEventHandler.ExecuteWhenFinished(Initialize);
+        }
+
+        public static void Initialize()
+        {
+            Patches.TryAllPatchesForSOS2(HarmonyInstance);
+        }
+    }
+}
